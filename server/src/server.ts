5df2a9c0391ffa9d1109f21086e32e5fea8b4bfd
alessandra <alessandraamontanher@gmail.com>
2020-06-01 09:22:14 -0300

    init server :tada:

---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+/*
+    1) npm install typescript -D
+    2) create config file for TS => npx tsc --init
+*/
+
+/*
+- when using TS, we must specify not only the library but also the definition (infos about
+    all of the functions that exist in express, all the returns, parameters etc);
+- there are libraries that separate into two libraries (one definition of types, other the code);
+
+npm install @types/express
+*/
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Working...");
+  // res.json(["A", "B", "C"]);
+});
+
+app.listen(3333);
+
+/**
+ * node src/server.ts is not going to work, because node only understands JS (this is a TS script)
+ * add other package => npm install ts-node -D
+ * npx executes a package that you've installed
+ * so, to run the app => npx ts-node src/server.ts
+ */
+
+/**
+ * Add NODEMON for hot reloading
+ * npm install ts-node-dev -D
+ * Add "dev": "ts-node-dev src/server.ts" in package.json file => npm run dev
+ */
